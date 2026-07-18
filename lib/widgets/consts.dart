727ede41ee83e6a51f385430cdc742d7027a918f
Abderrahmane Berramdan <berramdan.abderrahmane30@gmail.com>
2026-07-18 16:22:17 +0100

    fix: update CV link in consts.dart for accurate reference

--- lib/widgets/consts.dart
+++ lib/widgets/consts.dart
@@ -2,4 +2,4 @@ final String urlGtihub = "https://github.com/Abderrahmane-Berramdan";
 final String urlLinkedin =
     "https://www.linkedin.com/in/abderrahmane-berramdan-6bb225301/";
 final String urlCV =
-    "https://drive.google.com/file/d/1uGiYl9SUBKaQD8v7d6XLWS5v3B0Ncy3V/view?usp=drive_link";
+    "https://drive.google.com/file/d/1PV66ONWxTh4aUDWZdNt3UV2Rm8RIJpfz/view?usp=drive_link";
